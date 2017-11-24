9a16e0aec7cf68c312abf1b68a9d7079198d6214
Victor GUERAND <vguerand@e1r12p12.42.fr>
2017-11-24 14:30:39 +0100

    OUI JACKI

--- ft_lst_search_index.c
+++ ft_lst_search_index.c
@@ -6,7 +6,7 @@
 /*   By: vguerand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 13:39:31 by vguerand          #+#    #+#             */
-/*   Updated: 2017/11/24 14:07:13 by vguerand         ###   ########.fr       */
+/*   Updated: 2017/11/24 14:28:57 by vguerand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ t_list		*ft_lst_search_index(t_list *lst, size_t index)
 		if (lst->index == index)
 			return(lst);
 	}
+	return (NULL);
 }
 
