e24d7bf4fc6cc8880955137aeb23dc2eb048f9d6
Victor GUERAND <vguerand@e1r11p22.42.fr>
2017-11-28 13:24:56 +0100

    modif

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: vguerand <vguerand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 17:51:53 by vguerand          #+#    #+#             */
-/*   Updated: 2017/11/13 16:49:43 by vguerand         ###   ########.fr       */
+/*   Updated: 2017/11/28 13:24:39 by vguerand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,8 @@
 
 char	*ft_strnew(size_t size)
 {
-	size_t	i;
-	char	*copy;
+	char *str;
 
-	i = 0;
-	if (!(copy = ft_memalloc(size + 1)))
-	{
-		return (NULL);
-	}
-	while (i < size)
-	{
-		copy[i] = '\0';
-		i++;
-	}
-	return (copy);
+	str = (char *)ft_memalloc(size + 1);
+	return (str);
 }
